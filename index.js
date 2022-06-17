8365ec72f4c028db2715ad1a2bcb9d15cfb59706
Abhigyan Dhakal <abhigya.dhakal@gmail.com>
2022-06-17 06:48:50 +0545

    Installing Node packages and fixing minor backend errors

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ const app = express();
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.send("Yo whas poppin");
-});
-
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
